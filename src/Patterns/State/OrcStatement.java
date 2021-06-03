bc6ad7f74dc3deefdd658d736221acaf70f8a67a
mastifikator <mastifikator@yandex.ru>
2021-06-04 01:50:49 +0300

    Реализовал паттерн Состояние.

---
+++ src/Patterns/State/OrcStatement.java
@@ -0,0 +1,21 @@
+package Patterns.State;
+
+public abstract class OrcStatement {
+
+    Orc orc;
+
+    public OrcStatement(Orc orc) {
+        this.orc = orc;
+    }
+
+    public void orcBeat(){
+        System.out.println("Орк, палка, бить!" );
+    }
+
+    public void orcRun(){
+        System.out.println("Орк, бежать!" );
+    }
+
+    public abstract void orcCry();
+
+}
