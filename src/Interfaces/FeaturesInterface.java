81fa9c5c9e9764f2ece88c2da54aa5b336d53536
mastifikator <mastifikator@yandex.ru>
2021-07-09 14:13:46 +0300

    Проверял ограничения интерфейсов

--- src/Interfaces/FeaturesInterface.java
+++ src/Interfaces/FeaturesInterface.java
@@ -1,28 +1,19 @@
-import javax.sql.rowset.RowSetProvider;
-import java.io.*;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+package Interfaces;
 
 interface ITest {
-    static void giveMeTest(){
+    static void giveMeTest() {
         System.out.println("Я статический метод интерфейса");
     }
 
     public abstract void giveMeAbstract();
 
-    default void giveMeDefault(){
+    default void giveMeDefault() {
         System.out.println("Я дефолтный метод!");
     }
 }
 
 interface ITest2 {
-    static void giveMeTest(){
+    static void giveMeTest() {
         System.out.println("Я статический метод интерфейса");
     }
 
@@ -34,22 +25,22 @@ interface ITest2 {
 }
 
 
-public class Test implements ITest, ITest2 {
+public class FeaturesInterface implements ITest, ITest2 {
 
     @Override
     public void giveMeAbstract() {
         System.out.println("Я абстрактный метод!");
     }
 
-    static void giveMeTest(){
+    static void giveMeTest() {
         System.out.println("Я статический метод ласса Test");
     }
 
-    public static synchronized void main(String[] args){
+    public static synchronized void main(String[] args) {
 
         ITest.giveMeTest();
 
-        ITest test = new Test();
+        ITest test = new FeaturesInterface();
         test.giveMeDefault();
 
         test.giveMeAbstract();
